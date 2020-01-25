e66e15c8b13d79e7c61d5e74381f0877e1a82245
Ryan Krol <ryankr@hotmail.co.uk>
2020-01-25 17:05:28 +0000

    Add a component for plotting data
    * This will handle all of the plotting library interactions

---
+++ src/components/RatingsPlotter.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+
+import '../../node_modules/react-vis/dist/style.css';
+import {
+  XYPlot,
+  LineSeries,
+  VerticalGridLines,
+  HorizontalGridLines,
+  XAxis,
+  YAxis,
+} from 'react-vis';
+
+class RatingsPlotter extends Component {
+  // React seems to think that the props are always different, I think because
+  // the props are objects/arrays which don't seem to be compared properly in JS.
+  // Converting to JSON first and then comparing prevents work being redone every
+  // time somebody types anything into one of the inputs
+  shouldComponentUpdate(nextProps, nextState) {
+    return JSON.stringify(nextProps) !== JSON.stringify(this.props)
+  }
+
+  // Parses the series ratings into line data for the plot
+  updateLineSeries() {
+    const lineSeries = this.props.ratings.reduce((acc, seasonRatings, index) => {
+      const individualLineSeries = this.convertRatingsToLineSeries(seasonRatings, index)
+      acc.push(individualLineSeries)
+
+      return acc
+    }, [])
+
+    this.ratings = lineSeries
+  }
+
+
+  convertRatingsToLineSeries(seasonRatings, currentSeason) {
+    const lineSeriesData = seasonRatings.reduce((acc, item, index) => {
+      const dict = {}
+      dict.x = (index)/(seasonRatings.length) + currentSeason + 1
+      dict.y = item
+
+      acc.push(dict)
+      return acc
+    }, [])
+
+    return <LineSeries
+      data={lineSeriesData}
+      key={`currentSeason_${currentSeason}`}
+    />
+  }
+
+  render() {
+    this.updateLineSeries()
+    return (
+      <div className="App">
+        <XYPlot height={400} width={700}>
+        <VerticalGridLines />
+        <HorizontalGridLines />
+        <XAxis />
+        <YAxis />
+        {this.ratings}
+        </XYPlot>
+      </div>
+    );
+  }
+}
+
+export default RatingsPlotter;
