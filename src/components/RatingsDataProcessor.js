e367be8dfd5d143ae2d777f068a71d00edd0a450
Ryan Krol <ryankr@hotmail.co.uk>
2020-01-25 17:04:41 +0000

    Add a component for fetching series rating data
    * Having a class for this lets us separate out each
    part of the process, and reduce the amount of work any
    one thing is doing

---
+++ src/components/RatingsDataProcessor.js
@@ -0,0 +1,39 @@
+import fetch from "node-fetch"
+
+const API_CALL_TYPES = {
+  NAME: 'seriesName',
+  ID: 'seriesId',
+}
+
+class RatingsDataProcessor {
+  constructor(callType, apiInput) {
+    this.currentApiCallType = callType
+    this.apiInput = apiInput
+  }
+
+  async fetchSeasonsRatings() {
+    const endpoint = this.getApiEndpoint()
+
+    return fetch(endpoint).then((response) => {
+      return response.json()
+    }).then((ratingsData) => {
+      return Object.values(ratingsData.ratings)
+    })
+  }
+
+  getApiEndpoint() {
+    switch(this.currentApiCallType) {
+      case API_CALL_TYPES.NAME:
+        return `http://localhost:8000/ratings/name/${this.apiInput}`;
+      case API_CALL_TYPES.ID:
+        return `http://localhost:8000/ratings/id/${this.apiInput}`;
+      default:
+        throw new Error('Could not determine which API to use')
+    }
+  }
+}
+
+export {
+  API_CALL_TYPES,
+  RatingsDataProcessor,
+};
