7d286eff7ce935af25e9d1c1cb6e5d5000a99445
Ryan Krol <ryankr@hotmail.co.uk>
2020-08-02 17:08:40 +0100

    Add ability to easily switch between dev/prod endpoint

--- src/components/RatingsDataProcessor.js
+++ src/components/RatingsDataProcessor.js
@@ -1,8 +1,10 @@
-import fetch from "node-fetch"
+import fetch from 'node-fetch'
+
+import baseEndpoint from './../utils'
 
 const API_CALL_TYPES = {
   NAME: 'seriesName',
-  ID: 'seriesId',
+  ID: 'seriesId'
 }
 
 class RatingsDataProcessor {
@@ -14,32 +16,32 @@ class RatingsDataProcessor {
   async fetchSeasonsRatings() {
     const endpoint = this.getApiEndpoint()
 
-    return fetch(endpoint).then((response) => {
-      return response.json()
-    }).then((ratingsData) => {
-      if (ratingsData.Error) {
-        throw ratingsData
-      }
-      return ratingsData
-    }).catch((error) => {
-      console.log(error)
-      throw error
-    })
+    return fetch(endpoint)
+      .then(response => {
+        return response.json()
+      })
+      .then(ratingsData => {
+        if (ratingsData.Error) {
+          throw ratingsData
+        }
+        return ratingsData
+      })
+      .catch(error => {
+        console.log(error)
+        throw error
+      })
   }
 
   getApiEndpoint() {
-    switch(this.currentApiCallType) {
+    switch (this.currentApiCallType) {
       case API_CALL_TYPES.NAME:
-        return `/api/ratings/name/${this.apiInput}`;
+        return `${baseEndpoint()}/ratings/name/${this.apiInput}`
       case API_CALL_TYPES.ID:
-        return `/api/ratings/id/${this.apiInput}`;
+        return `${baseEndpoint()}/ratings/id/${this.apiInput}`
       default:
         throw new Error('Could not determine which API to use')
     }
   }
 }
 
-export {
-  API_CALL_TYPES,
-  RatingsDataProcessor,
-};
+export { API_CALL_TYPES, RatingsDataProcessor }
