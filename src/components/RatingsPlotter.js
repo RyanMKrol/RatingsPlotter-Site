91b256840d92178e7b05cca3f856f394b6e28be0
RyanMKrol <ryankrol.m@gmail.com>
2021-07-05 23:30:26 +0100

    Fix 10/10 rated episodes graph
    * Breaking bad was the example driving this fix. For some
    reason the entire graph gets inverted when a 10/10 episode
    comes along.
    * This can be fixed by making the episode slightly less than
    a 10 by removing 1/Infinity.

--- src/components/RatingsPlotter.js
+++ src/components/RatingsPlotter.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import '../../node_modules/react-vis/dist/style.css';
+import "../../node_modules/react-vis/dist/style.css";
 import {
   FlexibleWidthXYPlot,
   LineMarkSeries,
   VerticalGridLines,
   HorizontalGridLines,
   XAxis,
-  YAxis,
-} from 'react-vis';
+  YAxis
+} from "react-vis";
 
-import './RatingsPlotter.css'
+import "./RatingsPlotter.css";
 
 class RatingsPlotter extends Component {
   // React seems to think that the props are always different, I think because
@@ -18,54 +18,62 @@ class RatingsPlotter extends Component {
   // Converting to JSON first and then comparing prevents work being redone every
   // time somebody types anything into one of the inputs
   shouldComponentUpdate(nextProps, nextState) {
-    return JSON.stringify(nextProps) !== JSON.stringify(this.props)
+    return JSON.stringify(nextProps) !== JSON.stringify(this.props);
   }
 
   // Parses the series ratings into line data for the plot
   updateLineSeries() {
-    const lineSeries = this.props.ratings.reduce((acc, seasonRatings, index) => {
-      const individualLineSeries = this.convertRatingsToLineSeries(seasonRatings, index)
-      acc.push(individualLineSeries)
+    const lineSeries = this.props.ratings.reduce(
+      (acc, seasonRatings, index) => {
+        const individualLineSeries = this.convertRatingsToLineSeries(
+          seasonRatings,
+          index
+        );
+        acc.push(individualLineSeries);
 
-      return acc
-    }, [])
+        return acc;
+      },
+      []
+    );
 
-    this.ratings = lineSeries
+    this.ratings = lineSeries;
   }
-
 
   convertRatingsToLineSeries(seasonRatings, currentSeason) {
     const lineSeriesData = seasonRatings.reduce((acc, item, index) => {
-      const dict = {}
-      dict.x = (index)/(seasonRatings.length) + currentSeason + 1
-      dict.y = item
+      const dict = {};
+      dict.x = index / seasonRatings.length + currentSeason + 1;
 
-      acc.push(dict)
-      return acc
-    }, [])
+      // React-Vis does not like episodes that have a 10/10, so by taking away
+      // the tiniest amount from each value, the graphing library is happy while
+      // maintining the integrity of the scores as much as possible
+      dict.y = item - 1 / Infinity;
 
-    return <LineMarkSeries
-      data={lineSeriesData}
-      key={`currentSeason_${currentSeason}`}
-    />
+      acc.push(dict);
+      return acc;
+    }, []);
+
+    console.log(lineSeriesData);
+
+    return (
+      <LineMarkSeries
+        data={lineSeriesData}
+        key={`currentSeason_${currentSeason}`}
+      />
+    );
   }
 
   render() {
-    this.updateLineSeries()
+    this.updateLineSeries();
     const listOfTicks = this.props.ratings.map((_, index) => {
-      return `${index+1}`
-    })
+      return `${index + 1}`;
+    });
 
     return (
-      <FlexibleWidthXYPlot
-        height={450}
-        size={4}
-      >
+      <FlexibleWidthXYPlot height={450} size={4}>
         <VerticalGridLines />
         <HorizontalGridLines />
-        <XAxis
-          tickValues={listOfTicks}
-        />
+        <XAxis tickValues={listOfTicks} />
         <YAxis />
         {this.ratings}
       </FlexibleWidthXYPlot>
